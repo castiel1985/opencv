646c0079454fe868772b9750db34ddcedd0ea579
castiel_ubuntu <lfb2010@126.com>
2018-07-10 16:32:36 +0800

    app.py

--- txt/docker-compose/app.py
+++ txt/docker-compose/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
 @app.route('/')
-  def hello():
+def hello():
       count = redis.incr('hits')
       return 'Hello World! I have been seen {} times.\n'.format(count)
            

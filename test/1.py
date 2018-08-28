14384e059f76878329ff3d250b8023719447732e
castiel <lfb2010@126.com>
2018-08-28 16:41:39 +0800

    modify

--- test/1.py
+++ test/1.py
@@ -40,3 +40,34 @@ model = KerasRegressor(build_fn=create_model, epochs=200, batch_size=5, verbose=
 kfold = KFold(n_splits=10, shuffle=True, random_state=seed)
 results = cross_val_score(model, x, Y, cv=kfold)
 print('beaeline: %.2f (%.2f) mse' % (results.mean(), results.std()))
+
+steps = []
+steps.append(('standarize', StandardScaler()))
+steps.append(('mlp', model))
+
+pipeline = Pipeline(steps)
+kfold = KFold(n_splits=10, shuffle=True, random_state=seed)
+results = cross_val_score(pipeline, x, Y, cv=kfold)
+print('Standardize: %.2f (%.2f) mse' % (results.mean(), results.std()))
+
+# 调参数选择最优模型
+
+param_grid = {}
+param_grid['units_list'] = [[20], [13, 6]]
+param_grid['optimizer'] = ['rmsprop', 'adam']
+param_grid['init'] = ['glorot_uniform', 'normal']
+param_grid['epochs'] = [100, 200]
+param_grid['patch_size'] = [5, 20]
+
+scaler = StandardScaler()
+scaler_x = scaler.fit_transform(x)
+grid = GridSearchCV(estimator=model, param_grid=param_grid)
+results = grid.fit(scaler_x, Y)
+
+print('Best: %f using %s' % (results.best_score_, results.best_params_))
+means = results.cv_results_['mean_test_score']
+stds = results.cv_results_['std_test_score']
+params = results.cv_results_['params']
+
+for mean, std, param in zip(means, stds, params):
+    print('%f (%f) with : %r' % (mean, std, param))

7953b2ebca332e85791ebbaecc70a8c39ef934d3
castiel <lfb2010@126.com>
2018-09-14 16:54:49 +0800

    20180914

--- study/test.py
+++ study/test.py
@@ -1,8 +1,10 @@
 import tensorflow as tf
 
 a = tf.Variable(tf.random_normal([2, 3], stddev=1, seed=1))
-
+x = tf.Variable(tf.zeros([784, 10]))
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     b = sess.run(a)
+    y = sess.run(x)
     print(b)
+    print(x)

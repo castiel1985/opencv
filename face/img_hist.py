52c2adcc9d71c1e8f59ea3ad758c5eb6b04eb366
Castiel-MacBookpro15 <castiel@Fly.local>
2018-10-30 23:05:59 +0800

    add modify

--- face/img_hist.py
+++ face/img_hist.py
@@ -3,7 +3,7 @@ import numpy as np
 from matplotlib import  pyplot as plt
 
 img = cv2.imread('img/789.jpeg')
-
+print(img.size)
 
 #hist = cv2.calcHist([img],[0],None,[256],[0,256])
 #plt.hist(img.ravel(),256,[0,256])
